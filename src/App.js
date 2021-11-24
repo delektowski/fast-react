3c189d669140cab85715abde1fda438fc1374109
Marcin <delektowski@interia.pl>
2021-11-24 18:43:31 +0100

    Fast implementation for React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+    provideFASTDesignSystem,
+    fastCard,
+    fastButton,
+    fastRadio,
+    fastRadioGroup,
+    radioStyles
+
+} from '@microsoft/fast-components';
+import { provideReactWrapper } from '@microsoft/fast-react-wrapper';
+import { css } from '@microsoft/fast-element';
+import React from 'react';
+
+const { wrap } = provideReactWrapper(
+    React,
+    provideFASTDesignSystem()
+);
+
+export const FastCard = wrap(fastCard());
+export const FastButton = wrap(fastButton())
+export const FastRadio = wrap(fastRadio({
+    styles: (ctx, def) => css`
+      ${  radioStyles
+      (ctx, def)}
+      :host([aria-checked="true"]) .control {
+        background: blue;
+      }
+      :host([aria-checked="true"]:not([disabled])) .control:hover {
+        background: blue;
+      }
+    `,
+}),)
+export const FastRadioGroup = wrap(fastRadioGroup())
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>FAST React</h2>
+        <hr/>
+        <fast-radio-group value="mango" name="favorite-fruit">
+            <fast-radio value="apple">Apple</fast-radio>
+            <fast-radio value="mango">Mango</fast-radio>
+            <fast-radio value="orange">Orange</fast-radio>
+        </fast-radio-group>
     </div>
   );
 }
